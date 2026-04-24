670c9a8ac00dcaff9d859d7b52c7af45885d7ce6
loegcas <110066175+4shizzgndlf@users.noreply.github.com>
2026-04-24 11:50:14 +0200

    First commit

---
+++ src/main/java/dk/ek/bilabonnementprojekt/BilAbonnementProjektApplication.java
@@ -0,0 +1,14 @@
+package dk.ek.bilabonnementprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BilAbonnementProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilAbonnementProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/ek/bilabonnementprojekt/BilAbonnementProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.bilabonnementprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAbonnementProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
